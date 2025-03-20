ff008a3d0f1cc4dffd16540d2b5a2a2b33214c9d
Akash  Mapari <akash.mapari@kmccorp.in>
2025-03-20 13:43:01 +0530

    update the docusaurus.config

--- app/docusaurus-app/docusaurus.config.ts
+++ app/docusaurus-app/docusaurus.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: "https://streaman-docusarus.vercel.app/",
+  // https://streaman-docusarus.vercel.app/
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: "/docs/",
